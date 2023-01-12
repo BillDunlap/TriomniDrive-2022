dc0599faa2f196b8fc50dddc897970db84184034
BillDunlap <williamwdunlap@gmail.com>
2023-01-11 17:49:52 -0800

    Add set* routines to TuningVariables

--- TriomniDrive-2022/src/main/java/frc/robot/TuningVariables.java
+++ TriomniDrive-2022/src/main/java/frc/robot/TuningVariables.java
@@ -1,6 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Author: Bill Dunlap <williamwdunlap@gmail.com>, FRC 4173 (Team IMVERT), January 2023.
+// This software is free to use by anyone with no restrictions.
 
 package frc.robot;
 
@@ -9,7 +8,14 @@ import edu.wpi.first.wpilibj.Preferences;
 /** These things are like variables in Constants,
  * but can be set via the Preferences class (e.g.,
  * via Shuffleboard) and will be stored in flash
- * memory on the roborio.
+ * memory on the roborio.  The Preferences class
+ * stores values in flash (persistent) memory on
+ * the Roborio.  The TuningVariables class also
+ * stores default values in Java (nonpersistent)
+ * memory.
+ * 
+ * To get a tuning variable's value use the syntax
+ *   TuningVariables.variableName.get()
  * 
  * A current limitation is that the turning variables
  * must be doubles.
@@ -19,15 +25,33 @@ public enum TuningVariables {
     defaultSpinRate_DegreesPerSecond (40.0),
     defaultTravelRate_FeetPerSecond (2.0);
 
+    private double m_defaultValue;
+    /** Users cannot call an enum constructor directly;
+     * Java will call it for each variable listed above.
+     * Note the constructor will not change pre-existing
+     * values in flash memory.  Use setToDefaultValue or
+     * setAllToDefaultValues for that.
+     */
     private TuningVariables(double defaultValue){
         m_defaultValue = defaultValue;
         if (!Preferences.containsKey(name())) {
           Preferences.setDouble(name(), m_defaultValue);
         }
     }
-    private double m_defaultValue;
+    
     public double get() {
         return Preferences.getDouble(name(), m_defaultValue);
+    }
+    void set(double value) {
+        Preferences.setDouble(name(), value);
+    }
+    void setToDefaultValue() {
+        set(m_defaultValue);
+    }
+    static void setAllToDefaultValues() {
+        for(TuningVariables tv: TuningVariables.values()) {
+            tv.setToDefaultValue();
+        }
     }
     void remove(){
         Preferences.remove(name());
@@ -37,4 +61,5 @@ public enum TuningVariables {
             tv.remove();
         }
     }
+ 
 }
